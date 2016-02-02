04dfb244ba7b9c7e00c7bbad222f0a34f3118cbc
oppaitype <witawat.tuns@bumail.net>
2016-02-03 01:53:13 +0700

    Shooting shots

--- SpaceSample/Assets/Scripts/PlayerController.cs
+++ SpaceSample/Assets/Scripts/PlayerController.cs
@@ -9,23 +9,41 @@ public class Boundary
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
     public float speed;
     public float tilt;
     public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawm;
+    public float fireRate;
+
+    public float nextFire;
+
+    void Update ()
+    {
+        if (Input.GetButton("Firel") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            //         GameObject clone = 
+            Instantiate(shot, shotSpawm.position, shotSpawm.rotation);// as GameObject;
+        }
+    }
+
     void FixedUpdate ()
     {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        GetComponent.velocity = movement * speed;
+        rb.velocity = movement * speed;
 
-        GetComponent<Rigidbod>.position = new Vector3
+        rb.position = new Vector3
             (
                 Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
                 0.0f,
                 Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
             );
-        GetComponent<Rigidbody>.rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
     }
 }
