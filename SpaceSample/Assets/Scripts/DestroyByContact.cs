94fe762f73b7ffd57c0f579724dd28c54e8eec47
WITAWAT TUNSAKUNWIWATTANA <witawat.tuns@bulive.net>
2016-02-03 12:31:03 +0700

    Creating hazards

---
+++ SpaceSample/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	void OnTriggerExit(Collider other)
+	{
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
