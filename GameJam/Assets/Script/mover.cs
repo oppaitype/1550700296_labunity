166c9fba076c5bb895d8f5802ae48555cc1f9b5b
WITAWAT TUNSAKUNWIWATTANA <witawat.tuns@bulive.net>
2016-02-10 12:51:05 +0700

    GameJam

---
+++ GameJam/Assets/Script/mover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate ()
+	{
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			rb.AddForce (Vector3.up * speed);
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			rb.AddForce (Vector3.left * speed);
+		}
+	}
+}
