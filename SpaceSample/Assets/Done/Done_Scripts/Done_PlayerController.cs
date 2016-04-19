35092aad9220bad047cae00fe3fa928f2db084d6
oppaitype <witawat.tuns@bumail.net>
2016-04-20 03:43:45 +0700

    Mobile Development: Space Shooter to Mobile 47:11

--- SpaceSample/Assets/Done/Done_Scripts/Done_PlayerController.cs
+++ SpaceSample/Assets/Done/Done_Scripts/Done_PlayerController.cs
@@ -2,48 +2,81 @@
 using System.Collections;
 
 [System.Serializable]
-public class Done_Boundary 
+public class Done_Boundary
 {
-	public float xMin, xMax, zMin, zMax;
+    public float xMin, xMax, zMin, zMax;
 }
 
 public class Done_PlayerController : MonoBehaviour
 {
-	public float speed;
-	public float tilt;
-	public Done_Boundary boundary;
+    public float speed;
+    public float tilt;
+    public Done_Boundary boundary;
 
-	public GameObject shot;
-	public Transform shotSpawn;
-	public float fireRate;
-	 
-	private float nextFire;
-	
-	void Update ()
-	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
-		}
-	}
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public SimpleTouchPad touchPad;
+    public SimpleTouchAreaButton areaButton;
+    private Rigidbody rb;
+    private AudioSource audioSource;
 
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+    private float nextFire;
+    private Quaternion calibrationQuaternion;
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
-		
-		GetComponent<Rigidbody>().position = new Vector3
-		(
-			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-		);
-		
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
-	}
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+        CalibrateAccelerometer();
+    }
+
+    void Update()
+    {
+        if (areaButton.CanFire() && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audioSource.Play();
+        }
+    }
+
+ 
+    void CalibrateAccelerometer()
+    {
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    Vector3 FixAcceleration(Vector3 acceleration)
+    {
+        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+        return fixedAcceleration;
+    }
+
+    void FixedUpdate()
+    {
+        //      float moveHorizontal = Input.GetAxis ("Horizontal");
+        //      float moveVertical = Input.GetAxis ("Vertical");
+
+        //      Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        //      Vector3 accelerationRaw = Input.acceleration;
+        //      Vector3 acceleration = FixAcceleration (accelerationRaw);
+        //      Vector3 movement = new Vector3 (acceleration.x, 0.0f, acceleration.y);
+
+        Vector2 direction = touchPad.GetDirection();
+        Vector3 movement = new Vector3(direction.x, 0.0f, direction.y);
+        rb.velocity = movement * speed;
+
+       rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
 }
